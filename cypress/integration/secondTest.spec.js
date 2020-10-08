27a42aa75620e46d1a3544dad5873145172456d6
utdiscas <utdiscas@protonmail.com>
2020-10-08 15:43:48 +0200

    some sample cypress test added with  assertion

--- cypress/integration/secondTest.spec.js
+++ cypress/integration/secondTest.spec.js
@@ -4,14 +4,18 @@ describe('My second suite', () => {
     cy.visit('/')
     cy.contains('Forms').click();
     cy.contains('Form Layouts').click();
-    cy.get('[data-cy="signInButton"]')
 
-    cy.contains('Sign in')
 
-    cy.get('[status="warning"]', 'Sign in')
+    cy.contains('Sign in');
 
-    cy.get('#inputEmail3').parents('form').find('button')
+    cy.get('[status="warning"]', 'Sign in');
+
+    cy.get('#inputEmail3').parents('form').find('button');
       .should('contain', 'Sign in')
+      .parents('form')
+      .find('nb-checkbox')
+      .click();
+
   })
 
 })
